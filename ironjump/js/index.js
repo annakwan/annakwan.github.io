67a1b11f195c9c5f8fb102892ea58601be5f2be7
annakwan <cakewalk@ppaulvi.local>
2016-03-25 15:25:14 +0800

    game

--- ironjump/js/index.js
+++ ironjump/js/index.js
@@ -18,9 +18,9 @@ canvas.height = height;
 //Variables for game
 var platforms = [],
 	image = document.getElementById("sprite"),
-	player, platformCount = 7,
+	player, platformCount = 100,
 	position = 0,
-	gravity = 0.15,
+	gravity = 0,
 	animloop,
 	flag = 0,
 	menuloop, broken = 0,
